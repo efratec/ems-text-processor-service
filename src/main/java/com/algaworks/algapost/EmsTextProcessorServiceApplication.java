bd5c39cd263fe9edcac661f4136ef79b53f3eba3
Efraim Tenorio <efratec20.tenorio@gmail.com>
2025-07-02 14:40:17 -0300

    first commit

---
+++ src/main/java/com/algaworks/algapost/EmsTextProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algapost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsTextProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsTextProcessorServiceApplication.class, args);
+	}
+
+}
